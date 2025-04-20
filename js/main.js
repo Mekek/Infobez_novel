975c05fe0e9f35dbb64fdc856c880991eb750647
Mekek <kadilov2004@mail.ru>
2025-04-20 09:03:01 +0300

    disabled Sve and Back buttons

--- js/main.js
+++ js/main.js
@@ -33,7 +33,8 @@ $_ready (() => {
 
 	monogatari.init ('#monogatari').then (() => {
 		// 3. Inside the init function:
-		// monogatari.component ('quick-menu').removeButton ('Back');
+	monogatari.component ('quick-menu').removeButton ('Back');
+	monogatari.component ('quick-menu').removeButton ('Save');
 
 	});
 
